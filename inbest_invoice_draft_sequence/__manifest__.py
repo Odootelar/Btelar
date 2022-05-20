c8fdf8ff82089b90777d3de702c4e2bd9e2e22ce
demian-avila <demianavila87gmail.com>
2022-05-20 08:19:03 -0500

    update credits module

---
+++ inbest_invoice_draft_sequence/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Invoice draft sequence",
+    'version': "14.0.1.0",
+    'category': "Accounting/Accounting",
+    'author': 'Soltein SA de CV',
+    'website': 'http://www.soltein.net',
+    "license": "AGPL-3",
+    "description": """
+        Set the invoice sequence in draft state
+    """,
+    'depends': [
+        'account',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+}
